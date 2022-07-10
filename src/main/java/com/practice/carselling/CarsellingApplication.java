126bc4c974b9db9afecc0966ad05c1019a35010f
Ya1iX <ya1ix@bk.ru>
2022-07-11 01:06:00 +0300

    Initial commit

---
+++ src/main/java/com/practice/carselling/CarsellingApplication.java
@@ -0,0 +1,13 @@
+package com.practice.carselling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsellingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsellingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/carselling/CarsellingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.carselling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsellingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
